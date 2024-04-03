3090b3289554b779854c2f5205b1be973e51a5a9
jacek bastin <jacek.bastin@gmail.com>
2024-04-03 14:02:24 +0100

    Added Fetching CryptoCompare + showcase

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Proxying API requests to avoid CORS issues during development
+      '/api': {
+        target: 'https://min-api.cryptocompare.com/data',
+        changeOrigin: true, // this is important for handling CORS
+        rewrite: (path) => path.replace(/^\/api/, '') // rewrite '/api' to '' in the proxied request URL
+      },
+    },
+  },
+});
